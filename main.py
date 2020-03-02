1b14004d6fab9085be858bc9b7614c4f6547a85f
lanthora <nierunjie@gmail.com>
2020-03-02 15:39:10 +0800

    set log file

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from rssbot import RSSBot
 if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read('conf.ini')
-    
+
     if config.get("default", "loglevel") == 'ERROR':
         _level = logging.ERROR
     elif config.get("default", "loglevel") == 'INFO':
@@ -17,7 +17,10 @@ if __name__ == '__main__':
         _level = logging.DEBUG
 
     _format = "%(asctime)s - %(message)s"
-    logging.basicConfig(level=_level, format=_format)
+    _filename = "rss.log"
+    _filemode = "a"
+    logging.basicConfig(level=_level, format=_format,
+                        filename=_filename, filemode=_filemode)
 
     bot = RSSBot()
     bot.run()
